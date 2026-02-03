3ff51c5907af01b455b37b8063c4c066ed1cd750
Alson Basnet <basnetalson121@gmail.com>
2026-02-03 21:31:53 +0545

    This is the example of typecasting

---
+++ basic_practice/3.py
@@ -0,0 +1,6 @@
+# this is the example of typecasting
+a=int(input("Enter you number"))
+print(type(a))
+#We jsut changed the type of a so we will get string instead of integer.
+b=str(a)
+print(type(b))
