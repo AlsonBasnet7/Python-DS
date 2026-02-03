8941e5aff3f6a47a212ea451e3de98c48db0d859
Alson Basnet <basnetalson121@gmail.com>
2026-02-03 21:23:28 +0545

    This is the basic

---
+++ basic_practice/2.py
@@ -0,0 +1,10 @@
+user_name=str(input("Enter your name"))
+print(f"{user_name} is user's name")
+user_age=int(input("Enter your age"))
+print(f"{user_age} is user's age")
+is_user_student=int(input("If you are a student, Enter 1"))
+if is_user_student==1:
+    print("You are a student")
+else:
+    print("You are not a student")
+
