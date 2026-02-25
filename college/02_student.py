b808acd5e037e8ac3edee76743d1df32c8aa27f5
Alson Basnet <basnetalson121@gmail.com>
2026-02-25 15:18:00 +0545

    feat:total marks calculator:

---
+++ college/02_student.py
@@ -0,0 +1,12 @@
+subject1=int(input("Enter the first subject marks"))
+subject2=int(input("Enter the second subject marks"))
+subject3=int(input("Enter the third subject marks"))
+total_marks=subject1+subject2+subject3
+average_marks=total_marks/3
+print("The mark of first subject is",subject1)
+print("The mark of second subject is",subject2)
+print("The mark of third subject is",subject3)
+print("The toal marks is",total_marks)
+print("The average marks is",average_marks)
+
+
