405ec1b675e06a9b1133a53f1d79a010cbaf8895
Alson Basnet <basnetalson121@gmail.com>
2026-02-25 08:49:40 +0545

    feat:Some list methods

---
+++ basic_practice/22_sorting.py
@@ -0,0 +1,10 @@
+numbers=[2,4,5,6]
+numbers.append(20)
+print(numbers)
+numbers.remove(2)
+print(numbers)
+# inserting the name in the 1 index in the names list
+
+names=["alice","bob","charlie"]
+names.insert(1,"David")
+print(names)
