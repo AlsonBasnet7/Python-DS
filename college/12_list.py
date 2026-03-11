4493a823bfb67232e07ae55f77df6f985844dcd0
Alson Basnet <basnetalson121@gmail.com>
2026-03-11 15:12:30 +0545

    feat:college

---
+++ college/12_list.py
@@ -0,0 +1,4 @@
+# write a program that creates and oritns out a list containing only the unique elements from an existing list.
+list=[1,1,2,3,3,4,4,5,6,5,6]
+unique_list=set(list)
+print(unique_list)
