a6e8181aadf3957b5d4f2dee7960ffb78d51cb70
AlsonBasnet07 <basnetalson121@gmail.com>
2026-04-05 20:25:05 +0545

    feat:Class and objects

---
+++ OOPs/03_class.py
@@ -0,0 +1,9 @@
+#claass is basically a blue or template.
+#object is bascially a instance created from the template(class)
+class Employee:
+    brand ="Honda"
+    def get_salary(self):
+        print(self)
+        return 200
+e= Employee()# An object of the class employee is created here.
+print(e.get_salary())
