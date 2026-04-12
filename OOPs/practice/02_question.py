1e39627b85603cbad1dda9afd1a34f189fafcb07
Alson Basnet <basnetalson121@gmail.com>
2026-04-12 20:28:15 +0545

    feat:OOPs

---
+++ OOPs/practice/02_question.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self,name,age):
+        self.name =name
+        self.age=age
+    def name(self):
+        return self.name
+    def age(self):
+        return self.age
+p=Person("Alson",22)
+print(p.name)
+print(p.age)
+        
+        
